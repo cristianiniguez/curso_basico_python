a46914db9a3021ceb87d0d470883cfc95cdae5d8
criniguez <felizpc1@gmail.com>
2020-10-13 10:51:23 -0400

    conceptos basicos: operadores aritmeticos

---
+++ conceptos_basicos/operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+# Operadores Aritméticos en Python
+
+5 + 5 # 10 (Suma)
+5 - 4 # 1 (Resta)
+5 * 2 # 10 (Multiplicación)
+21 / 5 # 4.2 (División exacta)
+21 // 5 # 4 (División entera)
+21 % 5 # 1 (Resto)
+2 ** 2 # 4 (Potencia)
+
+# Para sacar raíz (por ejemplo raíz cuadrada de 9)
+
+# Primera forma
+9 ** .5
+
+# Segunda forma
+import math
+math.sqrt(9)
