e398934c66ad1c0f7f2a38eb2460a9dc9af659ee
criniguez <felizpc1@gmail.com>
2020-10-13 11:42:35 -0400

    conceptos basicos: operadores logicos y de comparacion

---
+++ conceptos_basicos/operadores_logicos_comparacion.py
@@ -0,0 +1,25 @@
+# Operadores lógicos
+
+es_estudiante = True
+trabaja = False
+
+es_estudiante and trabaja # False
+es_estudiante or trabaja # True
+not es_estudiante # False
+not trabaja # True
+
+# Operadores de comparación
+
+numero1 = 5
+numero2 = 5
+numero3 = 7
+
+numero1 == numero2 # True
+numero1 == numero3 # False
+numero1 != numero3 # True
+numero1 > numero3 # False
+numero1 < numero3 # True
+numero1 >= numero3 # False
+numero1 >= numero2 # True
+numero1 <= numero2 # True
+numero1 <= numero3 # True
