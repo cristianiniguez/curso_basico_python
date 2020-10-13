8fd8180b59cb31885b490c67c513537f0ff9c30e
criniguez <felizpc1@gmail.com>
2020-10-13 11:04:38 -0400

    conceptos basicos: variables

---
+++ conceptos_basicos/variables.py
@@ -0,0 +1,13 @@
+numero = 3
+print(numero) # 3
+
+numero1 = 5
+numero2 = 6
+numero1 + numero2 # 11
+
+numero1 = 12
+numero1 # 12 (Ya no 5)
+numero1 + numero2 # 18 (ya no 11)
+
+numero_resultado = numero1 + numero2
+numero_resultado # 18
