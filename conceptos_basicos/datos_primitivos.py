c87e9d3ea63162b21966b39dc31c3f15785ca50d
criniguez <felizpc1@gmail.com>
2020-10-13 11:17:27 -0400

    conceptos basicos: datos primitivos

---
+++ conceptos_basicos/datos_primitivos.py
@@ -0,0 +1,20 @@
+nombre = "Cristian"
+nombre # Cristian
+
+nombre2 = 'Mary'
+nombre2 # Mary
+
+nombre + nombre2 # CristianMary
+nombre + ' ' + nombre2 # Cristian Mary
+
+nombre * 4 # CristianCristianCristianCristian
+
+numero_decimal = 3.4
+numero_decimal # 3.4
+
+es_estudiante = True
+es_estudiante # True
+print(es_estudiante) # True
+
+trabaja = False
+trabaja # False
