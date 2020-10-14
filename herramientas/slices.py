f6a23ef66989ed5323e1ea50a4d7acd02313bf89
criniguez <felizpc1@gmail.com>
2020-10-14 13:37:36 -0400

    herramientas: trabajando con texto - silces

---
+++ herramientas/slices.py
@@ -0,0 +1,13 @@
+nombre = 'Cristian'
+
+nombre[0] # 'C'
+nombre[1] # 'r'
+
+nombre[0:3] # 'Cri'
+nombre[:3] # 'Cri'
+nombre[3:] # 'stian'
+nombre[1:7] # 'ristia'
+nombre[1:7:2] # 'rsi'
+nombre[::] # 'Cristian'
+nombre[1::3] # 'Csa'
+nombre[::-1] # 'naitsirC'
