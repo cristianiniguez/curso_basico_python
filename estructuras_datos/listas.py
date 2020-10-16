13105b434dd17e95df9027a52eb8777b36a3a2ed
criniguez <felizpc1@gmail.com>
2020-10-16 15:39:26 -0400

    estructuras: listas

---
+++ estructuras_datos/listas.py
@@ -0,0 +1,31 @@
+numero = 1
+numero  # 1
+
+numero = 4
+numero  # 4 (chau 1)
+
+numeros = [1, 3, 6, 8, 9, 45, 90]
+numeros  # [1, 3, 6, 8, 9, 45, 90]
+
+objetos = ['Hola', 3, 4.5, True]
+objetos  # ['Hola', 3, 4.5, True]
+
+objetos[1]  # 3
+objetos[0]  # 'Hola'
+objetos[3]  # True
+objetos[4]  # Error
+
+objetos.append(False)
+objetos  # ['Hola', 3, 4.5, True, False]
+
+objetos.append(1)
+objetos  # ['Hola', 3, 4.5, True, False, 1]
+
+objetos.pop(1)  # 3
+objetos  # ['Hola', 4.5, True, False, 1]
+
+for elemento in objetos:
+    print(elemento)  # 'Hola', 4.5, True, False, 1
+
+objetos[::-1]  # [1, False, True, 4.5, 'Hola']
+objetos[1:3]  # [4.5, True]
